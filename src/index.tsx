49c8e4d07bf7b161ec084d94b23e3be0e658ef74
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-10-30 22:00:54 -0700

    add css baseline from mui

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Board from "./components/Board";
 
 const App = () => (
   <div>
+    <CssBaseline />
     <Board />
   </div>
 );
