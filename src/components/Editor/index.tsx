78626193f5af4ac3015fe57917c0a89c6324b10e
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-11-02 23:48:16 -0800

    fix drawer animation

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,34 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Box, Button, SwipeableDrawer } from "@material-ui/core";
+import { Box, Button, Drawer } from "@material-ui/core";
 import { OwnProps } from "./types";
 
-const Editor: React.FC<OwnProps> = ({ isShowing, title, hide }) =>
-  isShowing
-    ? ReactDOM.createPortal(
-        <React.Fragment>
-          <SwipeableDrawer
-            anchor="right"
-            open={isShowing}
-            onClose={() => hide}
-            onOpen={() => hide}
-            BackdropProps={{ invisible: true }}
-          >
-            <Box m={2}>
-              <h1>{title}</h1>
-            </Box>
-            <Button
-              fullWidth={true}
-              color="primary"
-              variant="contained"
-              onClick={hide}
-            >
-              DONE
-            </Button>
-          </SwipeableDrawer>
-        </React.Fragment>,
-        document.body
-      )
-    : null;
+const Editor: React.FC<OwnProps> = ({ isShowing, title, hide }) => {
+  return (
+    <React.Fragment>
+      <Drawer
+        anchor="right"
+        open={isShowing}
+        onClose={() => hide}
+        BackdropProps={{ invisible: true }}
+      >
+        <Box m={2}>
+          <h1>{title}</h1>
+        </Box>
+        <Button
+          fullWidth={true}
+          color="primary"
+          variant="contained"
+          onClick={hide}
+        >
+          DONE
+        </Button>
+      </Drawer>
+    </React.Fragment>
+  );
+};
 
 export default Editor;
