fb26a10fcb87b6f14f193de14f70c05e27d8d944
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-11-17 11:24:47 -0800

    add menu options

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,58 @@
 import React from "react";
-import AddCircleIcon from "@material-ui/icons/AddCircle";
+import AddIcon from '@material-ui/icons/Add';
+import { Menu as MuiMenu, MenuItem, IconButton } from "@material-ui/core";
 import { MenuContainer } from "./index.style";
 
+const options = ["Text", "Image", "Container"];
+
+const ITEM_HEIGHT = 48;
+
 const Menu: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <MenuContainer>
-      <AddCircleIcon color="action" fontSize="large" />
+      <IconButton
+        aria-label="more"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <AddIcon color="action" fontSize="large" />
+      </IconButton>
+
+      <MuiMenu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: "20ch",
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem
+            key={option}
+            selected={option === "Pyxis"}
+            onClick={handleClose}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </MuiMenu>
     </MenuContainer>
   );
 };
